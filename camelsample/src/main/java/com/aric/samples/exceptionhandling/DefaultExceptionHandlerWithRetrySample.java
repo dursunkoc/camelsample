92449dcd5158dcc18b255a46cc5492c3fcc575cf
dursunkoc <dursunKoc@gmail.com>
2013-03-18 00:30:14 +0200

    Exception handling with Retry

---
+++ camelsample/src/main/java/com/aric/samples/exceptionhandling/DefaultExceptionHandlerWithRetrySample.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.aric.samples.exceptionhandling;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.SimpleRegistry;
+
+/**
+ * @author dursun
+ * 
+ */
+public class DefaultExceptionHandlerWithRetrySample {
+	private static final SimpleRegistry registry = new SimpleRegistry();
+	private static final CamelContext camelContext = new DefaultCamelContext(
+			registry);
+
+	private static final ProducerTemplate pt = camelContext
+			.createProducerTemplate();
+
+	public static void main(String[] args) throws Exception {
+		registry.put("orderService", OrderService.class);
+		camelContext.addRoutes(new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				errorHandler(defaultErrorHandler().maximumRedeliveries(5)
+						.redeliveryDelay(2000).backOffMultiplier(2));
+				from("direct:start").beanRef("orderService", "validateForTime")
+						.beanRef("orderService", "store");
+			}
+		});
+
+		camelContext.start();
+		pt.sendBody("direct:start", "product=1,customer=101");
+
+		Thread.sleep(30000);
+		camelContext.stop();
+	}
+}
--- camelsample/src/main/java/com/aric/samples/exceptionhandling/OrderService.java
+++ camelsample/src/main/java/com/aric/samples/exceptionhandling/OrderService.java
@@ -4,7 +4,7 @@ import org.apache.camel.Exchange;
 
 /**
  * @author dursun
- *
+ * 
  */
 public class OrderService {
 	public String validate(String input) {
@@ -15,14 +15,52 @@ public class OrderService {
 		}
 	}
 
+	public String validateForTime(String input, Exchange exchange) {
+		lapTime(exchange);
+		Long retry = getRetry(exchange);
+		if (retry < 3) {
+			setRetry(exchange, retry + 1);
+			System.out.println("input('" + input + "'); exchange('" + exchange
+					+ "') should be retried!");
+			throw new RuntimeException("input('" + input + "'); exchange('"
+					+ exchange + "') should be retried!");
+		}
+		return validate(input);
+	}
+
+	private void lapTime(Exchange exchange) {
+		long currentTimeMillis = System.currentTimeMillis();
+		Long lap = exchange.getProperty("lap",Long.class);
+		if(lap==null){
+			System.out.println("First Trial Time :>"+currentTimeMillis);
+		}else{
+			Long timePeriodAfterLastTrial = currentTimeMillis - lap;
+			System.out.println(timePeriodAfterLastTrial+" ms taken from last trial until now ('"+currentTimeMillis+"')");
+		}
+		exchange.setProperty("lap", currentTimeMillis);
+	}
+
+	private void setRetry(Exchange exchange, long retry) {
+		exchange.setProperty("retry", retry);
+	}
+
+	private Long getRetry(Exchange exchange) {
+		Long retry = exchange.getProperty("retry", Long.class);
+		if (retry == null) {
+			retry = 0l;
+		}
+		return retry;
+	}
+
 	public void store(String input) {
 		System.out.println("storing :> '" + input + "'");
 	}
-	
-	public void deadLetters(Exchange exchange){
+
+	public void deadLetters(Exchange exchange) {
 		Exception e = exchange.getProperty(Exchange.EXCEPTION_CAUGHT,
-                Exception.class);
-		System.out.println("dead letter :> '"+exchange+"' exception caught is:>");
+				Exception.class);
+		System.out.println("dead letter :> '" + exchange
+				+ "' exception caught is:>");
 		e.printStackTrace(System.out);
 	}
 }
